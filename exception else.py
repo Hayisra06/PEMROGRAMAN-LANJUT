c9e6adf5419a113be7bb136f530e3009666194d5
Hayisra <49136315+Hayisra06@users.noreply.github.com>
2019-10-08 15:40:07 +0700

    Create exception else.py

---
+++ exception else.py
@@ -0,0 +1,17 @@
+import sys
+def main():
+	print ("PROGRAM PEMBAGIAN BILANGAN")
+
+	a =  float(input("Masukan a: "))
+	b =  float(input("Masukan b: "))
+
+	try:
+		hasil = a/b
+	except ZeroDivisionError:
+		print ("\nERROR : Nilai b tidak boleh 0")
+	else:
+		print("\na : ", a)
+		print("b : ", b)
+		print("a / b = ", hasil)
+if __name__ == "__main__":
+	main()
